0ea31dacc199e54dacc543b56054d438e6134b73
Johnedel S. Mapa <johnedel.mapa@sun-asterisk.com>
2020-04-09 10:57:00 +0800

    Initialize Project

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import TabNavigator from './navigation/TabNavigator.js'
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <TabNavigator/>
+    );
+  }
+}
