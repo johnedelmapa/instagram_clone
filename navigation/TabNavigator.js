294cd0bf6fc351d633a6b33a68b35bab07add891
Johnedel S. Mapa <34313943+johnedelmapa@users.noreply.github.com>
2020-04-10 09:40:29 +0800

    Add Tabs Navigation Icons (#1)

--- navigation/TabNavigator.js
+++ navigation/TabNavigator.js
@@ -1,14 +1,59 @@
 import React from 'react';
 import { Text, View } from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import Home from '../screens/Home.js'
 import Search from '../screens/Search.js'
 import Activity from '../screens/Activity.js'
+import Upload from '../screens/Upload.js';
+import Profile from '../screens/Profile.js';
 import { createBottomTabNavigator, createAppContainer } from 'react-navigation';
 
 const TabNavigator = createBottomTabNavigator({
-  Home: Home,
-  Search: Search,
-  Activity: Activity
+  Home: {
+    screen: Home,
+    navigationOptions: {
+      tabBarLabel: ' ',
+      tabBarIcon: () => (
+        <Ionicons name='ios-home' size={32} />
+      )
+    }
+  },
+  Search: {
+    screen: Home,
+    navigationOptions: {
+      tabBarLabel: ' ',
+      tabBarIcon: () => (
+        <Ionicons name='ios-search' size={32} />
+      )
+    }
+  },
+  Upload: {
+    screen: Upload,
+    navigationOptions: {
+      tabBarLabel: ' ',
+      tabBarIcon: () => (
+        <Ionicons name='ios-add-circle-outline' size={32} />
+      )
+    }
+  },
+  Activity: {
+    screen: Activity,
+    navigationOptions: {
+      tabBarLabel: ' ',
+      tabBarIcon: ({focused}) => (
+        <Ionicons name={ focused ? 'ios-heart' : 'ios-heart-empty'} size={32} />
+      )
+    }
+  },
+  Profile: {
+    screen: Profile,
+    navigationOptions: {
+      tabBarLabel: ' ',
+      tabBarIcon: () => (
+        <Ionicons name='ios-person' size={32} />
+      )
+    }
+  },
 });
 
 export default createAppContainer(TabNavigator);
